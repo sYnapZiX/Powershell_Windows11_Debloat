562f23d742c99806af7035471230b12755da32fe
sYnapZiX <59868210+sYnapZiX@users.noreply.github.com>
2026-03-01 06:39:30 +0100

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -11,24 +11,9 @@ Write-Host " "
 Write-Host " "
 Write-Host "Step 1 of $totalsteps Raphire's Debloat Script..."
 Write-Host " "
-$choice = Read-Host "Use latest version from original repository? (y/n)"
-switch ($choice) {
-    y {
-        clear
-        Write-Host "Running Raphire's Debloat Script... (Latest Version)"
-        & ([scriptblock]::Create((irm "https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1")))
-    }
-    n {
-        clear
-        Write-Host "Running Raphire's Debloat Script... (Backup Version)"
-        & ([scriptblock]::Create((irm "https://raw.githubusercontent.com/sYnapZiX/Fork_Raphire_Win11Debloat/master/Get.ps1")))
-    }
-    default {
-        clear
-        exit
-    }
-}
-
+clear
+Write-Host "Running Raphire's Debloat Script... (Backup Version)"
+& ([scriptblock]::Create((irm "https://raw.githubusercontent.com/sYnapZiX/Fork_Raphire_Win11Debloat/master/Get.ps1")))
 clear
 
 Write-Host "$header"
